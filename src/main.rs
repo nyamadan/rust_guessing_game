9cd6db0122bab83e6d1c36181f9d5b307ab62009
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-02-27 23:46:26 +0900

    cmp

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -10,5 +19,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess:u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less      => println!("Too small"),
+        Ordering::Greater   => println!("Too big"),
+        Ordering::Equal     => println!("You win!"),
+    }
 }
